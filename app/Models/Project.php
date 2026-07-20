f77f324d56383b068e79842d80a99f9bf60e3bf8
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-07-20 23:47:12 +0200

    feat: Update Project and Type models to establish relationship and fillable properties

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,18 +9,22 @@ class Project extends Model
     protected $fillable = [
         'title',
         'description',
+        'type_id',
         'cover_image',
         'github_url',
         'project_url',
         'completed_at',
     ];
 
-     protected function casts(): array
+    protected function casts(): array
     {
         return [
             'completed_at' => 'date',
         ];
     }
 
-
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
