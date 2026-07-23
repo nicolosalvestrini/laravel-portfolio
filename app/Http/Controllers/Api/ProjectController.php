19e405a3b7e83c11dea715c911fefb2f41bae60e
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-07-23 23:44:04 +0200

    feat: Implement Project API endpoints and add CORS configuration

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with(['type', 'technologies'])
+            ->orderByDesc('created_at')
+            ->get();
+
+        return response()->json($projects);
+    }
+
+    public function show(Project $project)
+    {
+        $project->load(['type', 'technologies']);
+
+        return response()->json($project);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{project}', [ProjectController::class, 'show']);
